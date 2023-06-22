3ee99ecb559e1ed49d0c5737582203bddcc1462b
unknown <sebaslogo@gmail.com>
2023-06-21 19:46:15 -0500

    Finished ch 15

--- readme.md
+++ readme.md
@@ -3,6 +3,9 @@ My implementation of the Lox programming language from https://craftinginterpret
 gcc -o clox main.c chunk.c memory.c debug.c vm.c value.c
 
 - [x] 14 - Chunks of Bytecode
-- [ ] 15 - A Virtual Machine
+- [x] 15 - A Virtual Machine
+    - [x] 15.1
+    - [x] 15.2
+    - [x] 15.3
 - [ ] 16 - 
 - [ ] 17 - 
--- vm.c
+++ vm.c
@@ -31,6 +31,7 @@ Value pop() {
 static InterpretResult run() {
     #define READ_BYTE() (*vm.ip++) // increment the ptr
     #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()]) // increment ptr and read from that index of values array
+    #define BINARY_OP(op) do { double b = pop(); double a = pop(); push(a op b); } while(false) // do while lets us have multiple statements
 
     for (;;) {
 
@@ -55,6 +56,11 @@ static InterpretResult run() {
                 push(constant);
                 break;
             }
+            case OP_ADD: BINARY_OP(+); break;
+            case OP_SUBTRACT: BINARY_OP(-); break;
+            case OP_MULTIPLY: BINARY_OP(*); break;
+            case OP_DIVIDE: BINARY_OP(/); break;
+            case OP_NEGATE: push(-pop()); break;
             case OP_RETURN: {
                 printValue(pop());
                 printf("\n");
@@ -65,6 +71,7 @@ static InterpretResult run() {
 
     #undef READ_BYTE
     #undef READ_CONSTANT
+    #undef BINARY_OP
 }
 
 InterpretResult interpret(Chunk* chunk) {
