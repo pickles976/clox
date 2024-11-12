a28abc6ff7bd6ffe188cbffcb2e2a4091499b0c4
Sebastian Gomez <sebaslogo@gmail.com>
2024-11-11 20:41:05 -0600

    Added some tokens

--- compiler.c
+++ compiler.c
@@ -4,6 +4,19 @@
 #include "compiler.h"
 #include "scanner.h"
 
-void compile(const cahr* source) {
+void compile(const char* source) {
     initScanner(source);
+    int line = - 1;
+    for (;;) {
+        Token token = scanToken();
+        if (token.line != line) {
+            printf("%4d ", token.line);
+            line = token.line;
+        } else {
+            printf("   | ");
+        }
+        printf("%2d '%.*s'\n", token.type, token.length, token.start);
+
+        of (token.type == TOKEN_EOF) break;
+    }
 }
